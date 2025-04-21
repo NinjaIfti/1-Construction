69c9110a94884ce7a7b5e6effdf80d805ea55f16
Iftikhar Ahmed <ifti3061@gmail.com>
2025-04-22 01:08:04 +0600

    removed

--- public/resources/js/app.js
+++
@@ -1,5 +0,0 @@
-// Basic navbar functionality (keeping it minimal)
-document.addEventListener('DOMContentLoaded', function() {
-    // Mobile menu toggle handled by Bootstrap
-    // No additional JavaScript needed for the simple navbar
-}); 
